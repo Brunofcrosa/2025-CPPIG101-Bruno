59c866455b87c7d86d1d88820ceb376e722d3715
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-04-26 20:32:52 -0300

    adicionando um CRUD na aba cliente

--- cliente/models.py
+++ cliente/models.py
@@ -1,11 +1,24 @@
 from django.db import models
-
+from stdimage.models import StdImageField
 # Create your models here.
-class Cliente(models.Model):
+class Pessoa(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
     telefone = models.CharField(max_length=15)
-    endereco = models.TextField()
+    foto = StdImageField('Foto', upload_to='pessoas', delete_orphans=True, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.nome
+    
+class Cliente(Pessoa):
+    endereco = models.CharField('Endereço', max_length=100, help_text='Endereço completo')
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
 
     def __str__(self):
         return self.nome
--- cliente/urls.py
+++ cliente/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import ClienteListView
+from .views import ClientesView, ClienteAddView, ClienteUpdateView, ClienteDeleteView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('clientes/', ClienteListView.as_view(), name='Cliente'),
-]
+    path('clientes/', ClientesView.as_view(), name='cliente'),
+    path('cliente/adicionar/', ClienteAddView.as_view(), name='cliente_adicionar'),
+    path('cliente/<int:pk>/editar/', ClienteUpdateView.as_view(), name='cliente_editar'),
+    path('cliente/<int:pk>/apagar/', ClienteDeleteView.as_view(), name='cliente_apagar'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
