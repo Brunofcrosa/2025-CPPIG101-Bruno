3f482c32359163e569fedcf84b66502b80f4fb46
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-05-15 18:07:47 -0300

    arrumando index

--- home/views.py
+++ home/views.py
@@ -10,8 +10,10 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['total_imoveis'] = Imovel.objects.count()
+        context['imoveis_recentes'] = Imovel.objects.all()[:4]
         context['total_clientes'] = Cliente.objects.count()
         context['total_corretores'] = Corretor.objects.count()
         context['total_visitas'] = Visita.objects.count()
+        context['visitas_recentes'] = Visita.objects.all()[:4]
 
         return context
