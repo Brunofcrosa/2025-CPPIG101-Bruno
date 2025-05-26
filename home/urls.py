eb92128be36e48719efed74fd8b30b95989cc779
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-05-26 20:07:07 -0300

    fix

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import IndexView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
