7ae0260e5ffa3259aff3631dee5aacadf756654b
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-06-11 19:55:44 -0300

    imovel

--- imovel/forms.py
+++ imovel/forms.py
@@ -1,3 +1,4 @@
+# 2025-CPPIG101-Bruno/imovel/forms.py
 from django import forms
 from .models import Imovel
 
@@ -34,3 +35,29 @@ class ImovelModelForm(forms.ModelForm):
             'proprietario': {'required': 'Proprietário é obrigatório.'},
         }
         
+    def clean(self):
+        cleaned_data = super().clean() 
+        
+        area_total = cleaned_data.get('areaTotal')
+        area_privativa = cleaned_data.get('areaPrivativa')
+        area_util = cleaned_data.get('areaUtil')
+
+        if area_privativa is not None and area_total is not None:
+            if area_privativa > area_total:
+                self.add_error(
+                    'areaPrivativa', 
+                    'A Área Privativa não pode ser maior que a Área Total.'
+                )
+        
+
+        if area_util is not None and area_total is not None:
+            if area_util > area_total:
+                self.add_error(
+                    'areaUtil', 
+                    'A Área Útil não pode ser maior que a Área Total.'
+                )
+
+        if area_total is None and (area_privativa is not None or area_util is not None):
+            self.add_error('areaTotal', 'A Área Total é obrigatória se a Área Privativa ou Útil forem preenchidas.')
+            
+        return cleaned_data
