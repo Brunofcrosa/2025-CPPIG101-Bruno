4e016a5b9bae679bb0a2e929160d9d5498490d4a
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-06-16 19:28:39 -0300

    permissions-corretor

--- corretores/views.py
+++ corretores/views.py
@@ -3,11 +3,13 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.core.paginator import Paginator
 from django.contrib import messages
 from django.urls import reverse_lazy
-
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from .models import Corretor
 from .forms import CorretorModelForm
 
-class CorretoresView(ListView):
+class CorretoresView(PermissionRequiredMixin, ListView):
+    permission_required = 'corretores.view_corretor'
+    permission_denied_message = 'Visualizar corretor'
     model = Corretor
     template_name = 'corretores.html'
 
@@ -25,21 +27,27 @@ class CorretoresView(ListView):
         else:
             return messages.info(self.request, 'Nenhum corretor encontrado com esse nome')
 
-class CorretorAddView(SuccessMessageMixin, CreateView):
+class CorretorAddView(PermissionRequiredMixin, SuccessMessageMixin, CreateView):
+    permission_required = 'corretores.add_corretor'
+    permission_denied_message = 'Adicionar corretor'
     model = Corretor
     form_class = CorretorModelForm
     template_name = 'corretor_form.html'
     success_url = reverse_lazy('corretor')
     success_message = 'corretor cadastrado com sucesso!'
 
-class CorretorUpdateView(SuccessMessageMixin, UpdateView):
+class CorretorUpdateView(PermissionRequiredMixin, SuccessMessageMixin, UpdateView):
+    permission_required = 'corretores.update_corretor'
+    permission_denied_message = 'Alterar corretor'
     model = Corretor
     form_class = CorretorModelForm
     template_name = 'corretor_form.html'
     success_url = reverse_lazy('corretor')
     success_message = 'corretor alterado com sucesso!'
 
-class CorretorDeleteView(SuccessMessageMixin, DeleteView):
+class CorretorDeleteView(PermissionRequiredMixin, SuccessMessageMixin, DeleteView):
+    permission_required = 'corretores.delete_corretor'
+    permission_denied_message = 'Excluir corretor'
     model = Corretor
     template_name = 'corretor_apagar.html'
     success_url = reverse_lazy('corretor')
