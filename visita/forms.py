b7e546c6e45b6ad01aa11eb39b3db8d35bfb7f8d
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-07-05 12:16:41 -0300

    calendario e ajuste regra de negocio

--- visita/forms.py
+++ visita/forms.py
@@ -1,34 +1,64 @@
 from django import forms
+import datetime
+from datetime import timedelta
 from .models import Visita
+
 
 class VisitaModelForm(forms.ModelForm):
     class Meta:
         model = Visita
-        fields = ['imovel', 'corretor', 'cliente', 'data']
-        
+        fields = ['imovel', 'corretor', 'cliente', 'data', 'hora']
+
+        widgets = {
+            'data': forms.DateInput(
+                attrs={
+                    'type': 'date',
+                    'min': datetime.date.today().isoformat()
+                }
+            ),
+            'hora': forms.TimeInput(attrs={'type': 'time'}),
+        }
+
         error_messages = {
             'imovel': {'required': 'É um campo obrigatório'},
             'corretor': {'required': 'O corretor é um campo obrigatório'},
             'cliente': {'required': 'O cliente é um campo obrigatório'},
             'data': {'required': 'A data é um campo obrigatório'},
+            'hora': {'required': 'A hora é um campo obrigatória'},
         }
 
     def clean(self):
         cleaned_data = super().clean()
-        imovel = cleaned_data.get('imovel') 
-        corretor = cleaned_data.get('corretor') 
+        imovel = cleaned_data.get('imovel')
+        corretor = cleaned_data.get('corretor')
         data = cleaned_data.get('data')
-        
+        hora = cleaned_data.get('hora')
+
         if imovel:
             if imovel.status_imovel() == 'Em Confirmação de Venda':
                 raise forms.ValidationError(
                     "Não é possível agendar visitas para este imóvel, pois ele está em processo de confirmação de venda."
                 )
-            
-        if data:
-            if Visita.objects.filter(corretor=corretor, data=data).exists():
-                raise forms.ValidationError(
-                    "O corretor já tem uma visita agendada para esta data."
-                )
-            
+
+        if corretor and imovel and data and hora:
+            proposed_dt = datetime.datetime.combine(data, hora)
+
+            visitas_com_potencial_conflito = Visita.objects.filter(
+                corretor=corretor,
+                imovel=imovel,
+                data=data
+            )
+            if self.instance and self.instance.pk:
+                visitas_com_potencial_conflito = visitas_com_potencial_conflito.exclude(pk=self.instance.pk)
+
+            for outra_visita in visitas_com_potencial_conflito:
+                outra_visita_dt = datetime.datetime.combine(outra_visita.data, outra_visita.hora)
+                diferenca_tempo = abs(proposed_dt - outra_visita_dt)
+
+                if diferenca_tempo < timedelta(minutes=30):
+                    self.add_error(
+                        'hora',
+                        f"O corretor já possui uma outra visita agendada em {outra_visita.data.strftime('%d/%m/%Y')} às {outra_visita.hora.strftime('%H:%M')}! (Mínimo exigido é de 30 minutos!)"
+                    )
+                    break
         return cleaned_data
--- visita/models.py
+++ visita/models.py
@@ -1,15 +1,15 @@
-import re
 from django.db import models
 from stdimage.models import StdImageField
 from imovel.models import Imovel
 from cliente.models import Cliente
-# Create your models here.
+
 class Visita(models.Model):
     corretor = models.ForeignKey('corretores.Corretor', on_delete=models.CASCADE, related_name='corretor', null=True)
     imovel = models.ForeignKey('imovel.Imovel', on_delete=models.CASCADE, related_name='imovel', null=True)
     cliente = models.ForeignKey('cliente.Cliente', on_delete=models.CASCADE, related_name='cliente', null=True)
     data = models.DateField('Data', help_text='Data da visita', null=True)
-    codigoVisita = models.CharField('Código da Visita', max_length=10, unique=True, help_text='Código único da visita') 
+    hora = models.TimeField('Hora', help_text='Hora da visita', null=True, blank=True) 
+    codigoVisita = models.CharField('Código da Visita', max_length=10, unique=True, help_text='Código único da visita')
 
     class Meta:
         permissions = (('fechar_agendamento', 'Permite fazer o fechamento de uma visita'),)
@@ -17,20 +17,20 @@ class Visita(models.Model):
         verbose_name_plural = 'Visitas'
 
     def save(self, *args, **kwargs):
-        if not self.codigoVisita:  
-        
-            iniciais = self.__class__.__name__[:2].upper()  
-            finais = self.__class__.__name__[-2:].upper()   
-        
-        
+        if not self.codigoVisita:
+
+            iniciais = self.__class__.__name__[:2].upper()
+            finais = self.__class__.__name__[-2:].upper()
+
+
             ultima_visita = Visita.objects.order_by('-codigoVisita').first()
-        
-            if ultima_visita:  
+
+            if ultima_visita:
                 numero = int(ultima_visita.codigoVisita[2:-2]) + 1
-            else:  
+            else:
                 numero = 1
-            
-        
+
+
             self.codigoVisita = f"{iniciais}{numero:03d}{finais}"
-    
-        super().save(*args, **kwargs)  
+
+        super().save(*args, **kwargs)
